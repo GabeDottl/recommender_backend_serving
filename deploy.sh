9ee922e1da4774e0b76c613395f5f657729a8849
Gabe Dottl <gabedottl@gmail.com>
2020-04-11 11:49:28 -0700

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # https://cloud.google.com/compute/docs/containers/deploying-containers#updating_a_container_on_a_vm_instance
-docker build --tag serving:latest
+docker build --tag serving:latest .
 docker tag serving:latest gcr.io/recommender-270613/serving
 docker push gcr.io/recommender-270613/serving
 gcloud compute instances update-container serving-1 --container-image gcr.io/recommender-270613/serving
